58c12dfb0bc53eb23c6986c78731ecd74de1f0be
sho_kudo_home <sho_kudo_home@192.168.0.14>
2021-06-15 22:55:51 +0900

    adminMaintenanceSvlの内部処理作成

--- src/main/java/controller/AdminMaintenanceSvl.java
+++ src/main/java/controller/AdminMaintenanceSvl.java
@@ -1,12 +1,18 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.IdealException;
+import model.MenuType;
 
 /**
  * Servlet implementation class AdminMaintenanceSvl
@@ -38,8 +44,35 @@ public class AdminMaintenanceSvl extends HttpServlet {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
+		RequestDispatcher rd = null;
+
+		try{
+			//セッション情報がnullならホームページへ
+			HttpSession session = request.getSession(true);
+			if(session.getAttribute("adminInfo") == null){
+				rd = request.getRequestDispatcher("/home.jsp");
+				rd.forward(request, response);
+			}
 
 
+			//adminリストを取得してセッションにセットする
+			ArrayList<adminMaintenance> alam = MenuType.getAllType();
+			request.setAttribute("admList",alam);
+
+			//ここまで問題なければadminMaintenance画面に戻って再表示
+			rd = request.getRequestDispatcher("/adminMaintenance.jsp");
+
+
+			//独自例外が発生したらメッセージ取得＆管理者処理画面に遷移
+		}catch(IdealException e) {
+//			}catch(Exception e) {
+			IdealException ie = new IdealException(IdealException.ERR_NO_DB_EXCEPTION);
+			request.setAttribute("msg", ie.getMsg());
+//			System.out.println(msg);
+			rd = request.getRequestDispatcher("/adminIndex.jsp");
+			rd.forward(request, response);
+
+		}
 	}
 
 }
